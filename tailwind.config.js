077811edc3a2d99fab8f26da94363ca61f8310d0
amirk1998 <amirk1998.pv@gmail.com>
2023-01-08 20:04:31 +0330

    Navbar and Product grid created

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    // require('@tailwindcss/typography'),
+    // require('@tailwindcss/line-clamp'),
+    // require('@tailwindcss/aspect-ratio'),
+    // require('@tailwindcss/forms')({
+    //   strategy: 'base', // only generate global styles
+    //   strategy: 'class', // only generate classes
+    // }),
+  ],
+};
